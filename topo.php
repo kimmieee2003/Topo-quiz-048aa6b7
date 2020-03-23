304d77d9dca89dff7504fa77eeb7ef4f716e1de0
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-23 09:38:44 +0100

    verbeter

--- topo.php
+++ topo.php
@@ -1,9 +1,9 @@
 <?php
-$hoofdstad = array("Japan" => "Tokyo", "Mexico" => "Mexico-city", "USA" => "Washington D.C.", "India" => "New Delhi", "Zuid-Korea" => "Seoul", "China" => "Peking", "Nigeria" => "Abuja", "Argentina" => "Buenos Aires", "Egypt" => "Cairo", "UK" => "London");
+$hoofdstad = array("Japan" => "Tokyo", "Mexico" => "Mexico-city", "USA" => "Washington D.C.",);
 foreach($hoofdstad as $land => $boo){
     echo "wat is de hoofdstad van " .$land.PHP_EOL; 
     $stad = readline();
-    if($stad == $boo){
+    if($stad == $boo) {
         echo "dat klopt ga maar door" .PHP_EOL;
     }else{
         echo "Helaas $stad is niet de hoofdstad van $land";
